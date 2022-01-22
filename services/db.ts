6dd4f55dccf4e873ae344a6e28daa509f9d21a3a
vovi-z <81784608+vovi-z@users.noreply.github.com>
2022-01-21 18:27:06 -0600

    Moved database variables to .env file for security

--- services/db.ts
+++ services/db.ts
@@ -1,11 +1,11 @@
 import { Pool, PoolClient } from 'pg'
 
 const pool = new Pool({
-    host: "localhost",
-    user: "postgres",
-    password: "root",
-    database: "zenon-test",
-    port: 5432,
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_DATABASE,
+    port: Number(process.env.DB_PORT)
 })
 
 interface IClientHandler {
