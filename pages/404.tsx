aa0e7c45a1ba6910b7668958f1a3b403540cf494
vovi-z <81784608+vovi-z@users.noreply.github.com>
2022-01-24 13:19:36 -0600

    added searchbar to 404 page

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,11 +1,15 @@
 import Layout from '../components/Layout'
 import styles from './404.module.scss'
 import Link from 'next/link'
+import Searchbar from '../components/Searchbar'
 
 export default function ErrorPage() {
     return (
         <Layout>
             <div className={styles.main}>
+                <div className={styles.searchBarWrapper}>
+                    <Searchbar />
+                </div>
                 <h1>Error 404! Search not found.</h1>
                 <Link href='/'>
                     <a className={styles.text}>Return to Home Page</a>
