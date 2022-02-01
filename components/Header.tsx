2ecd5c636e2558cbe734ab132efbf6744de30fef
vovi-z <81784608+vovi-z@users.noreply.github.com>
2022-01-31 23:57:30 -0600

    added texture to background of header

--- components/Header.tsx
+++ components/Header.tsx
@@ -2,8 +2,23 @@ import Head from 'next/head'
 import Link from 'next/link'
 import Image from 'next/image'
 import styles from './Header.module.scss'
+import { useEffect, useState } from 'react'
 
 export default function Header() {
+
+    const [isDesktop, setIsDesktop] = useState(false)
+
+    useEffect(() => {
+        setIsDesktop(window.innerWidth > 1450)
+
+        const updateMedia = () => {
+            setIsDesktop(window.innerWidth > 1450)
+        }
+
+        window.addEventListener('resize', updateMedia)
+        return () => window.removeEventListener('resize', updateMedia)
+    })
+
     return (
         <nav>
             <div className={styles.header}>
@@ -13,28 +28,50 @@ export default function Header() {
                             <Image src="/banner.png" width="63" height="29" alt="Zenon Scraper Logo"/>
                         </a>
                     </Link>
-                    <div className={styles.options}>
-                        <Link href='/'>
-                            <a className={styles.optionsText}>Home</a>
-                        </Link>
-                        <Link href='/momentumlist/1'>
-                            <a className={styles.optionsText}>Momentums</a>
-                        </Link>
-                        <Link href='/accountblocklist/1'>
-                            <a className={styles.optionsText}>Account Blocks</a>
-                        </Link>
-                        <Link href='/tokenlist'>
-                            <a className={styles.optionsText}>Tokens</a>
-                        </Link>
-                        <Link href='/resources'>
-                            <a className={styles.optionsText}>Resources</a>
-                        </Link>
-                        <Link href='/donate'>
-                            <a className={styles.optionsText}>Donate</a>
-                        </Link>
-                    </div>
+                    <NavigationBar isDesktop={isDesktop} />
                 </div>
             </div>
        </nav>
+    )
+}
+
+function NavigationBar({ isDesktop }) {
+    if (isDesktop) {
+        return <DesktopNavigationBar />
+    } else {
+        return <MobileNavigationBar />
+    }
+}
+
+function DesktopNavigationBar() {
+    return (
+        <div className={styles.options}>
+            <Link href='/'>
+                <a className={styles.optionsText}>Home</a>
+            </Link>
+            <Link href='/momentumlist/1'>
+                <a className={styles.optionsText}>Momentums</a>
+            </Link>
+            <Link href='/accountblocklist/1'>
+                <a className={styles.optionsText}>Account Blocks</a>
+            </Link>
+            <Link href='/tokenlist'>
+                <a className={styles.optionsText}>Tokens</a>
+            </Link>
+            <Link href='/resources'>
+                <a className={styles.optionsText}>Resources</a>
+            </Link>
+            <Link href='/donate'>
+                <a className={styles.optionsText}>Donate</a>
+            </Link>
+        </div>
+    )
+}
+
+function MobileNavigationBar() {
+    return (
+        <div className={styles.mobileNavigation}>
+            
+        </div>
     )
 }
