d2dcd1f2044f8e22aed8889b88461123adbaa180
vovi-z <81784608+vovi-z@users.noreply.github.com>
2022-02-01 18:27:12 -0600

    added a spacer component to all pages via Layout

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,12 +1,14 @@
 import Header from './Header'
 import Footer from './Footer'
 import styles from './Layout.module.scss'
+import Spacer from './Spacer'
 
 export default function Layout({ children }) {
     return (
         <div className={styles.pagecontainer}>
             <Header />
             <main className={styles.main}>{children}</main>
+            <Spacer />
             <Footer />
         </div>
     )
