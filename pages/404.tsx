17597162ab9613c08cb24cb0059c5ea713a8518b
vovi-z <81784608+vovi-z@users.noreply.github.com>
2022-02-03 00:43:43 -0600

    updated 404 page UI

--- pages/404.tsx
+++ pages/404.tsx
@@ -10,10 +10,16 @@ export default function ErrorPage() {
                 <div className={styles.searchBarWrapper}>
                     <Searchbar />
                 </div>
-                <h1>Error 404! Search not found.</h1>
-                <Link href='/'>
-                    <a className={styles.text}>Return to Home Page</a>
-                </Link>
+                <div className={styles.card}>
+                    <div className={styles.cardHeader}>
+                        <div className={styles.cardHeaderLeft}>
+                            <h1 className={styles.cardTitle}>Error 404! Search not found.</h1>
+                            <Link href='/'>
+                                <a className={styles.cardSubtitle}>Return to Home Page</a>
+                            </Link>
+                        </div>
+                    </div>
+                </div>
             </div>
         </Layout>
     )
