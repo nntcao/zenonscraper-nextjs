df4de58b7ffcb1b450f2c810d1fb25ad3ae015c3
vovi-z <81784608+vovi-z@users.noreply.github.com>
2022-04-01 00:35:50 -0500

    Renamed SubtextMedium function

--- components/GenericComponents/SubtextMedium/SubtextMedium.tsx
+++ components/GenericComponents/SubtextMedium/SubtextMedium.tsx
@@ -1,6 +1,6 @@
 import styles from './SubtextMedium.module.scss'
 
-export default function TextMedium(props) {
+export default function SubtextMedium(props) {
     return (
         <p className={`${props.className} ${styles.textMedium}`}>{ props.children }</p>
     )
