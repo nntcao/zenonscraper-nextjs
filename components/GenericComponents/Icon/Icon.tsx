13ec75f8103b59a0536a915f632f7a056393fc0f
vovi-z <81784608+vovi-z@users.noreply.github.com>
2022-04-01 16:54:14 -0500

    Cleaned up CSS of HeroSection and styling of Icon

--- components/GenericComponents/Icon/Icon.tsx
+++ components/GenericComponents/Icon/Icon.tsx
@@ -4,16 +4,12 @@ import styles from './Icon.module.scss'
 export default function Icon({ src, srcHover=undefined, width, height, style={}, alt=''}) {
     return (
         <div className={styles.iconWrapper} style={{ width: width, height: height}}>
-            <div className={`${styles.image}`}
-                    style={style}
-            >
+            <div className={`${styles.image}`} style={style}>
                 <Image src={src} width={width} height={height} alt={alt} />
             </div>
             {
                 srcHover &&
-                <div className={`${styles.image} ${styles.top}`}
-                    style={style}
-                >
+                <div className={`${styles.image} ${styles.top}`} style={style}>
                     <Image src={srcHover} width={width} height={height} />
                 </div>
             }
