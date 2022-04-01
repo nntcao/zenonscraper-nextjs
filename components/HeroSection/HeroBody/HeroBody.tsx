bc72392f1de8af424d49db34f3bf829322df61c4
vovi-z <81784608+vovi-z@users.noreply.github.com>
2022-03-31 19:54:05 -0500

    Refactored HeroBody and modified address/text icon

--- components/HeroSection/HeroBody/HeroBody.tsx
+++ components/HeroSection/HeroBody/HeroBody.tsx
@@ -1,16 +1,28 @@
 import Title from "../../GenericComponents/Title/Title"
 import TextMedium from "../../GenericComponents/TextMedium/TextMedium"
 import styles from './HeroBody.module.scss'
-import SearchBarWithText from "../SearchBarWithText/SearchBarWithText"
+import AddressIcon from '../../Icons/AddressIcon/AddressIcon'
+import SearchBar from '../../SearchBar/SearchBar'
+import SubtextMedium from '../../GenericComponents/SubtextMedium/SubtextMedium'
+import EmbeddedTextIcon from '../../Icons/EmbeddedTextIcon/EmbeddedTextIcon'
 
 export default function HeroBody({ title, description }) {
     return (
         <div className={styles.heroBody}>
             <div className={styles.heroText}>
-                <Title>{ title }</Title>
-                <TextMedium>{ description }</TextMedium>
+                <Title>{title}</Title>
+                <TextMedium>{description}</TextMedium>
             </div>
-            <SearchBarWithText />
+            <div className={styles.searchBarWrapper}>
+                <SearchBar />
+                <div className={styles.searchTextWrapper}>
+                    <SubtextMedium>Search Examples:</SubtextMedium>
+                    <div className={styles.iconWrapper}>
+                        <AddressIcon />
+                        <EmbeddedTextIcon />
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
