88757f2d19ed00f719c1c9c459c15d6edd7751a6
vovi-z <81784608+vovi-z@users.noreply.github.com>
2022-04-03 18:01:30 -0500

    Added divs for charts/tables in DataSection

--- components/Sections/DataSection/DataSection.tsx
+++ components/Sections/DataSection/DataSection.tsx
@@ -6,9 +6,13 @@ export default function DataSection(props) {
         <section className={styles.section}>
             <div className={styles.container}>
                 <GeneralDashboard />
-
-                
-
+                <div className={styles.charts}>
+                    {/* <Chart /> */}
+                    {/* <Chart /> */}
+                </div>
+                <div className={styles.tables}>
+                    
+                </div>
             </div>
         </section>
     )
