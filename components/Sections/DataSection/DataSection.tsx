be533da52e16dfb98f513c20b0c4490e2ffb6a2f
vovi-z <81784608+vovi-z@users.noreply.github.com>
2022-04-05 21:14:26 -0500

    Added LatestBlockTable to DataSection

--- components/Sections/DataSection/DataSection.tsx
+++ components/Sections/DataSection/DataSection.tsx
@@ -1,3 +1,6 @@
+import Table from '../../GenericComponents/Table/Table'
+import TableBody from '../../GenericComponents/Table/TableBody/TableBody'
+import LatestBlocksTable from '../../Tables/LatestBlocksTable/LatestBlocksTable'
 import GeneralDashboard from '../GeneralDashboard/GeneralDashboard'
 import styles from './DataSection.module.scss'
 
@@ -11,7 +14,7 @@ export default function DataSection(props) {
                     {/* <Chart /> */}
                 </div>
                 <div className={styles.tables}>
-                    
+                    <LatestBlocksTable/>
                 </div>
             </div>
         </section>
