0f938659f42f5e9f3c2fdd20bf05c1df1e8a3526
vovi-z <81784608+vovi-z@users.noreply.github.com>
2022-04-05 21:07:44 -0500

    Added styling on Card component

--- components/GenericComponents/Card/Card.tsx
+++ components/GenericComponents/Card/Card.tsx
@@ -1,8 +1,8 @@
 import styles from './Card.module.scss'
 
-export default function Card({ children=undefined, className='' }) {
+export default function Card({ children=undefined, className='', style={} }) {
     return (
-        <div className={`${styles.card} ${className}`}>
+        <div className={`${styles.card} ${className}`} style={style}>
             { children }
         </div>
     )
