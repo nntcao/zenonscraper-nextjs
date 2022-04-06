f0ccb41fe4ff93cbe3579461272da46d8a1de3d8
vovi-z <81784608+vovi-z@users.noreply.github.com>
2022-04-05 21:08:03 -0500

    Added className on Subtitle and modified font-weight/size

--- components/GenericComponents/Subtitle/Subtitle.tsx
+++ components/GenericComponents/Subtitle/Subtitle.tsx
@@ -1,7 +1,7 @@
 import styles from './Subtitle.module.scss'
 
-export default function Subtitle({ children }) {
+export default function Subtitle({ children, className }) {
     return (
-        <h2 className={styles.subtitle}> { children } </h2>
+        <h2 className={`${className} ${styles.subtitle}`}> { children } </h2>
     )
 }
