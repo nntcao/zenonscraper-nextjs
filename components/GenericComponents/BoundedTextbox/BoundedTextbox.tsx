8aa051e41d6755a6b13cd2be29369986426289ff
vovi-z <81784608+vovi-z@users.noreply.github.com>
2022-04-08 07:37:12 -0500

    Fixed BoundedTextbox CSS sizing

--- components/GenericComponents/BoundedTextbox/BoundedTextbox.tsx
+++ components/GenericComponents/BoundedTextbox/BoundedTextbox.tsx
@@ -1,10 +1,10 @@
 import TextMedium from '../TextMedium/TextMedium'
 import styles from './BoundedTextbox.module.scss'
 
-export default function BoundedTextbox({ children }) {
+export default function BoundedTextbox({ children, style={} }) {
     return (  
-        <div className={styles.box}>
-            <TextMedium>{ children }</TextMedium>
+        <div className={styles.box} style={style}>
+            <TextMedium style={{ textAlign: 'center' }}>{ children }</TextMedium>
         </div>
     )
 }
