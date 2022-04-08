d644fac9e0a9cef014cc28b1dfd4940879244a23
vovi-z <81784608+vovi-z@users.noreply.github.com>
2022-04-08 07:38:22 -0500

    Updated Text Components to include styles prop

--- components/GenericComponents/TextMedium/TextMedium.tsx
+++ components/GenericComponents/TextMedium/TextMedium.tsx
@@ -2,6 +2,6 @@ import styles from './TextMedium.module.scss'
 
 export default function TextMedium(props) {
     return (
-        <p className={`${props.className} ${styles.textMedium}`}>{ props.children }</p>
+        <p className={`${props.className} ${styles.textMedium}`} style={props.style}>{ props.children }</p>
     )
 }
--- components/GenericComponents/TextSemibold/TextSemibold.tsx
+++ components/GenericComponents/TextSemibold/TextSemibold.tsx
@@ -2,6 +2,6 @@ import styles from './TextSemibold.module.scss'
 
 export default function TextSemibold(props) {
     return (
-        <p className={`${props.className} ${styles.textSemibold}`}>{ props.children }</p>
+        <p className={`${props.className} ${styles.textSemibold}`} style={props.style}>{ props.children }</p>
     )
 }
