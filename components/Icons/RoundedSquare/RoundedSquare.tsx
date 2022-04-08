7d663ae872d135a5b7dbccc38c86c52d1f8211e9
vovi-z <81784608+vovi-z@users.noreply.github.com>
2022-04-08 07:38:51 -0500

    Updated RoundedSquare to have consistent CSS

--- components/Icons/RoundedSquare/RoundedSquare.tsx
+++ components/Icons/RoundedSquare/RoundedSquare.tsx
@@ -1,10 +1,10 @@
 import TextSemibold from '../../GenericComponents/TextSemibold/TextSemibold'
 import styles from './RoundedSquare.module.scss'
 
-export default function RoundedSquare({ children }) {
+export default function RoundedSquare({ children, style={} }) {
     return (
-        <div className={styles.block}>
-            <TextSemibold> { children } </TextSemibold>
+        <div className={styles.block} style={style}>
+            <TextSemibold style={{alignText: 'center'}}> { children } </TextSemibold>
         </div>
     )
 }
