6c155403ce194a750a2cf9a06b93306dd2183b62
vovi-z <81784608+vovi-z@users.noreply.github.com>
2022-05-12 20:37:27 -0500

    Created PrivacySection

---
+++ components/Sections/PrivacySection/PrivacySection.tsx
@@ -0,0 +1,37 @@
+import styles from './PrivacySection.module.scss'
+import InformationSection from '../InformationSection/InformationSection'
+import TextMedium from '../../GenericComponents/TextMedium/TextMedium'
+import Title from '../../GenericComponents/Title/Title'
+import Image from 'next/image'
+
+export default function PrivacySection(props) {
+    return (
+        <InformationSection 
+            text={
+                <div className={styles.text}>
+                    <Title>
+                        Privacy is Our Paramount
+                    </Title>
+                    <div className={styles.textBody}>
+                        <TextMedium>
+                            Zenon Scraper is dedicated to blockchain search engine privacy.
+                        </TextMedium>
+                        <br />
+                        <TextMedium>
+                            Unlike most blockchain explorers and cryptocurrency companies, we do not share any personal data with third-party analytics companies or ad networks.
+                        </TextMedium>   
+                        <br />
+                        <TextMedium>
+                            When using our services you do not risk any personal identifiable information becoming public.
+                        </TextMedium>   
+                    </div>
+                </div>
+            }
+            image={
+                <div className={styles.imageWrapper}>
+                    <Image src="/PrivacyImage.png" width={450} height={450}/>
+                </div>
+            }
+        />
+    )
+}
