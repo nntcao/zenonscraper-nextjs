a062f636a848048990942f065073e53ff0951dc9
vovi-z <81784608+vovi-z@users.noreply.github.com>
2022-05-12 21:00:44 -0500

    Added optional parameter on Subtitle

--- components/GenericComponents/Subtitle/Subtitle.tsx
+++ components/GenericComponents/Subtitle/Subtitle.tsx
@@ -1,6 +1,6 @@
 import styles from './Subtitle.module.scss'
 
-export default function Subtitle({ children, className }) {
+export default function Subtitle({ children, className={} }) {
     return (
         <h2 className={`${className} ${styles.subtitle}`}> { children } </h2>
     )
