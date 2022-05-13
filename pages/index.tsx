0f135b9fbc2b078f112a672f601afa517300ab5a
vovi-z <81784608+vovi-z@users.noreply.github.com>
2022-05-12 20:37:53 -0500

    Added ApiSection/PrivacySection to landing page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,8 @@
 import HeroSection from "../components/Sections/HeroSection/HeroSection";
 import DataSection from "../components/Sections/DataSection/DataSection";
 import AboutSection from "../components/Sections/AboutSection/AboutSection";
+import ApiSection from "../components/Sections/ApiSection/ApiSection";
+import PrivacySection from "../components/Sections/PrivacySection/PrivacySection";
 
 export default function Index(props) {
     return (
@@ -8,6 +10,8 @@ export default function Index(props) {
             <HeroSection />
             <DataSection />
             <AboutSection />
+            <ApiSection />
+            <PrivacySection />
         </>
     )
 }
